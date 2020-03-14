7825482617c0e4682315d9b15f82ba8cea139918
Renat <suleymanov_renat@mail.ru>
2020-03-15 00:19:16 +0300

    правки

--- src/add.js
+++ src/add.js
@@ -17,55 +17,37 @@ class Character {
 
 class Bowman extends Character {
    constructor(name) {
-     super(name);
-     this.type = "Bowman";
-     this.attack = 25;
-     this.defence = 25;
+     super(name, "Bowman", 25, 25);
    }
 }
 
 class Swordsman extends Character {
   constructor(name) {
-    super(name);
-    this.type = "Swordsman";
-    this.attack = 40;
-    this.defence = 40;
+    super(name, "Swordsman", 40, 40);
   }
 }
 
 class Magician extends Character {
   constructor(name) {
-    super(name);
-    this.type = "Magician";
-    this.attack = 10;
-    this.defence = 40;
+    super(name, "Magician", 10, 40);
   }
 }
 
 class Undead extends Character {
   constructor(name) {
-    super(name);
-    this.type = "Undead";
-    this.attack = 25;
-    this.defence = 25
+    super(name, "Undead", 25, 25);
   }
 }
 
 class Zombie extends Character {
   constructor(name) {
-    super(name);
-    this.type = "Zombie";
-    this.attack = 40;
-    this.defence = 10;
+    super(name, "Zombie", 40, 10);
   }
 }
 
 class Daemon extends Character {
   constructor(name) {
-    super(name);
-    this.type = "Daemon";
-    this.attack = 10;
-    this.defence = 40;
+    super(name, "Daemon", 10, 40);
   }
 }
 
